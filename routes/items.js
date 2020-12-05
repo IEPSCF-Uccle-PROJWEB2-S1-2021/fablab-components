6b71a1af7a781956cdf50899791eec1076929f6a
Francois ROLAND <froland@froland.be>
2020-12-05 18:48:19 +0100

    Validate item form.

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = new express.Router();
+const { body, validationResult } = require('express-validator');
+const createError = require('http-errors');
 
 class Item {
   constructor(description) {
@@ -10,15 +12,23 @@ class Item {
 const catalog = [];
 
 router.get('/new', (req, res, next) => {
-  res.render('item_form', { title: 'Encodage d\'un article' });
+  res.render('item_form', { title: "Encodage d'un article" });
 });
 
-router.post('/', (req, res, next) => {
-  const description = req.body.description;
-  const newItem = new Item(description);
-  catalog.push(newItem);
-  console.log(catalog);
-  res.redirect('/items/new');
-});
+router.post(
+  '/',
+  [body('description').not().isEmpty().trim().escape()],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(createError(404));
+    };
+    const description = req.body.description;
+    const newItem = new Item(description);
+    catalog.push(newItem);
+    console.log(catalog);
+    res.redirect('/items/new');
+  }
+);
 
 module.exports = router;

51b7df206a9822d4b374b49ef15c2d98a3dbbdb4
Francois ROLAND <froland@froland.be>
2020-12-12 15:42:19 +0100

    Implement search item.

--- models/catalog.js
+++ models/catalog.js
@@ -6,6 +6,22 @@ class Catalog {
   add(item) {
     this.items.push(item);
   }
+
+  search(search) {
+    const matchingItems = this.items.filter((item) => {
+      return item.description.indexOf(search) > -1;
+    });
+    matchingItems.sort((item1, item2) => {
+      if (item1.description < item2.description) {
+        return -1;
+      } else if (item1.description > item2.description) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return matchingItems;
+  }
 }
 
 class Item {
--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = new express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, query, validationResult } = require('express-validator');
 const createError = require('http-errors');
 const { Item, catalog } = require('../models/catalog');
 
@@ -15,12 +15,25 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return next(createError(404));
-    };
+    }
     const description = req.body.description;
     const newItem = new Item(description);
     catalog.add(newItem);
     console.log(catalog);
     res.redirect('/items/new');
+  }
+);
+
+router.get(
+  '/search',
+  [query('search').optional({ checkFalsy: true }).trim().escape()],
+  (req, res, next) => {
+    const search = req.query.search;
+    let items = [];
+    if (search !== undefined && search !== '') {
+      items = catalog.search(search);
+    }
+    res.render('item_search', { title: "Catalogue d'articles", items });
   }
 );
 
